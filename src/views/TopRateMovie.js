132cd91cd1ce36be52bb8f22f3e069b8884bcb35
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-11-22 22:41:06 +0700

    implement topRatedMovies, then render using CardMovie

--- src/views/TopRateMovie.js
+++ src/views/TopRateMovie.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+// components
+import CardMovie from "../components/CardMovie";
+// actions
+import { fetchTopRatedMovies } from "../redux/actions/movieAction";
 
 const TopRateMovie = () => {
-  return <div>TopRateMovie</div>;
+  const dispatch = useDispatch();
+
+  const topRatedMovies = useSelector((state) => state.movie.topRatedMovies);
+
+  useEffect(() => {
+    dispatch(fetchTopRatedMovies());
+  }, []);
+
+  return (
+    <div>
+      <div class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
+        {topRatedMovies &&
+          topRatedMovies.map((movie) => (
+            <CardMovie key={movie.id} movie={movie} />
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default TopRateMovie;
