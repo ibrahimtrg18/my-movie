0f9eb41cee12f4c1d8bf4b1a6408abdd3cb6e4ce
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-11-22 22:40:25 +0700

    dispatch fetching genre

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useDispatch } from "react-redux";
 // components
 import Layout from "./components/Layout";
 import TopRateMovie from "./views/TopRateMovie";
+// actions
+import { fetchGenresMovie } from "./redux/actions/movieAction";
 import "./App.css";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchGenresMovie());
+  }, []);
+
   return (
     <BrowserRouter>
       <Layout>
