cdca1d90bf42e7cbf54e30e08feccd34f908b24c
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-11-22 22:40:12 +0700

    setup redux for genre

--- src/redux/actions/movieAction.js
+++ src/redux/actions/movieAction.js
@@ -9,8 +9,26 @@ export const fetchTopRatedMovies =
         `${constants.BASE_URL}/movie/top_rated?api_key=${constants.API_KEY}&page=${page}`
       );
       const data = await res.json();
-      dispatch({ type: movieType.FETCH_TOP_RATED_MOVIES, payload: data });
+      dispatch({
+        type: movieType.FETCH_TOP_RATED_MOVIES,
+        payload: data.results,
+      });
     } catch (e) {
       return e;
     }
   };
+
+export const fetchGenresMovie = () => async (dispatch) => {
+  try {
+    const res = await fetch(
+      `${constants.BASE_URL}/genre/movie/list?api_key=${constants.API_KEY}`
+    );
+    const data = await res.json();
+    dispatch({
+      type: movieType.FETCH_GENRES_MOVIE,
+      payload: data.genres,
+    });
+  } catch (e) {
+    return e;
+  }
+};
--- src/redux/reducers/movieReducer.js
+++ src/redux/reducers/movieReducer.js
@@ -2,12 +2,16 @@ import movieType from "../types/movieTypes";
 
 const INITIAL_STATE = {
   topRatedMovies: [],
+  genres: [],
 };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case movieType.FETCH_TOP_RATED_MOVIES: {
       return { ...state, topRatedMovies: action.payload };
+    }
+    case movieType.FETCH_GENRES_MOVIE: {
+      return { ...state, genres: action.payload };
     }
     default:
       return state;
