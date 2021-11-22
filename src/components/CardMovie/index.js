1fe65cd9747756a5a9832cd522d41b026e5c856b
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-11-22 22:56:50 +0700

    replace attribute class to className

--- src/components/CardMovie/index.js
+++ src/components/CardMovie/index.js
@@ -11,41 +11,41 @@ const CardMovie = (props) => {
   const genres = useSelector((state) => state.movie.genres);
 
   return (
-    <div class="movie-item c-card block bg-white shadow-md hover:shadow-xl rounded-lg overflow-hidden">
-      <div class="relative pb-48 overflow-hidden">
+    <div className="movie-item c-card block bg-white shadow-md hover:shadow-xl rounded-lg overflow-hidden">
+      <div className="relative pb-48 overflow-hidden">
         <img
-          class="image absolute inset-0 h-full w-full object-cover"
+          className="image absolute inset-0 h-full w-full object-cover"
           src={`${constants.BASE_URL_POSTER}${poster_path}`}
           alt=""
         />
       </div>
-      <div class="p-4">
-        <h2 class="mt-2 mb-2  font-bold">{title}</h2>
-        <p class="text-sm description">{overview}</p>
+      <div className="p-4">
+        <h2 className="mt-2 mb-2  font-bold">{title}</h2>
+        <p className="text-sm description">{overview}</p>
       </div>
-      <div class="p-4 border-t border-b text-xs text-gray-700">
+      <div className="p-4 border-t border-b text-xs text-gray-700">
         {genres &&
           genres.length > 0 &&
           genre_ids.map((genre_id) => {
             const { name } = genres.find((genre) => genre.id === genre_id);
             if (name) {
               return (
-                <span class="inline-block px-2 py-1 leading-none bg-orange-200 text-orange-800 rounded-full font-semibold uppercase tracking-wide text-xs">
+                <span className="inline-block px-2 py-1 leading-none bg-orange-200 text-orange-800 rounded-full font-semibold uppercase tracking-wide text-xs">
                   {name}
                 </span>
               );
             }
           })}
       </div>
-      <div class="p-4 flex items-center text-sm text-gray-600">
-        <span class="mr-auto">Vote: {vote_count || 0}</span>
+      <div className="p-4 flex items-center text-sm text-gray-600">
+        <span className="mr-auto">Vote: {vote_count || 0}</span>
         {[...Array(5)].map((v, i) => {
           if (i < Math.floor(vote_average / 2)) {
             return (
               <svg
                 viewBox="0 0 24 24"
                 xmlns="http://www.w3.org/2000/svg"
-                class="h-4 w-4 fill-current text-yellow-500"
+                className="h-4 w-4 fill-current text-yellow-500"
               >
                 <path d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z"></path>
               </svg>
@@ -55,7 +55,7 @@ const CardMovie = (props) => {
               <svg
                 viewBox="0 0 24 24"
                 xmlns="http://www.w3.org/2000/svg"
-                class="h-4 w-4 fill-current text-gray-400"
+                className="h-4 w-4 fill-current text-gray-400"
               >
                 <path d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z"></path>
               </svg>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,23 +3,23 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div class="min-h-full">
-      <nav class="bg-gray-800">
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div class="flex items-center justify-between h-16">
-            <div class="flex items-center">
-              <div class="flex-shrink-0">
+    <div className="min-h-full">
+      <nav className="bg-gray-800">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between h-16">
+            <div className="flex items-center">
+              <div className="flex-shrink-0">
                 <img
-                  class="h-8 w-8"
+                  className="h-8 w-8"
                   src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
                   alt="Workflow"
                 />
               </div>
-              <div class="hidden md:block">
-                <div class="ml-10 flex items-baseline space-x-4">
+              <div className="hidden md:block">
+                <div className="ml-10 flex items-baseline space-x-4">
                   <Link
                     to="/movie/top_rated"
-                    class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
+                    className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
                     aria-current="page"
                   >
                     Top rated movies
@@ -27,56 +27,56 @@ const Navbar = () => {
 
                   <Link
                     to="/movie/upcoming"
-                    class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                   >
                     Upcoming movies
                   </Link>
 
                   <Link
                     to="/"
-                    class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                   >
                     Now playing movies
                   </Link>
 
                   <Link
                     to="/"
-                    class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                   >
                     Popular movies
                   </Link>
 
                   <Link
                     to="/"
-                    class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                   >
                     Popular TV shows
                   </Link>
 
                   <Link
                     to="/"
-                    class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                   >
                     Top rated TV shows
                   </Link>
 
                   <Link
                     to="/"
-                    class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                   >
                     On the air TV shows
                   </Link>
 
                   <Link
                     to="/"
-                    class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                   >
                     Airing today TV shows
                   </Link>
 
                   <Link
                     to="/"
-                    class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                   >
                     Popular people
                   </Link>
