bff24ea74c262b88847ecfe308bbe64f8a6f5b37
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-11-23 15:07:56 +0700

    create modal component

---
+++ src/components/Modal/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Modal = (props) => {
+  const { children } = props;
+
+  return (
+    <div
+      class="fixed z-10 inset-0 overflow-y-auto"
+      aria-labelledby="modal-title"
+      role="dialog"
+      aria-modal="true"
+    >
+      <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+        <div
+          class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+          aria-hidden="true"
+        />
+
+        <div class="absolute top-1/2 -translate-x-1/2 -translate-y-1/2 inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all my-8 sm:my-0 sm:align-middle sm:max-w-lg sm:w-full">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
