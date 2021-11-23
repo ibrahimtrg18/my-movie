404d341bfe6088765cbfc1e14d0ce9c3b3f5d769
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-11-23 19:22:49 +0700

    create Input component

---
+++ src/components/Input.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Input = (props) => {
+  const { children, label, customClass, ...restProps } = props;
+
+  if (label) {
+    return (
+      <label>
+        {label}
+        <input
+          {...restProps}
+          className={`bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-800 ${customClass}`}
+        >
+          {children}
+        </input>
+      </label>
+    );
+  }
+
+  return (
+    <input
+      {...restProps}
+      className={`bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-800 ${customClass}`}
+    >
+      {children}
+    </input>
+  );
+};
+
+export default Input;
