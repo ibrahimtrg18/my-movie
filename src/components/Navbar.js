f365793f57acac6c24277226156bc2ab5c30bddc
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-11-23 23:35:27 +0700

    improving UI responsive menu add toggle menu change logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,6 +7,7 @@ import { fetchAccount, logoutAccount } from "../redux/actions/authAction";
 const Navbar = () => {
   const dispatch = useDispatch();
   const [toggle, setToggle] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
   const auth = useSelector((state) => state.auth);
   const account = useSelector((state) => state.auth.account);
 
@@ -20,26 +21,44 @@ const Navbar = () => {
     dispatch(logoutAccount());
   };
 
-  console.log("debug ", { auth, account });
-
   return (
     <div className="min-h-full">
       <nav className="bg-gray-800">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex items-center justify-between h-16">
-            <div className="flex items-center w-full">
-              <div className="flex-shrink-0">
-                <img
-                  className="h-8 w-8"
-                  src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-                  alt="Workflow"
-                />
+          <div className="block sm:flex items-center justify-between h-full">
+            <div className="block sm:flex items-center w-full">
+              <div className="flex flex-shrink-0 py-2 items-center">
+                <div onClick={() => setShowMenu(!showMenu)}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="#ffffff"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="feather feather-menu"
+                  >
+                    <line x1="3" y1="12" x2="21" y2="12"></line>
+                    <line x1="3" y1="6" x2="21" y2="6"></line>
+                    <line x1="3" y1="18" x2="21" y2="18"></line>
+                  </svg>
+                </div>
+                <span className="text-3xl font-bold text-white ml-2">
+                  Movie
+                </span>
               </div>
-              <div className="hidden block sm:flex justify-between w-full">
-                <div className="ml-10 flex items-baseline space-x-4">
+              <div
+                className={`${
+                  showMenu ? "block sm:flex" : "hidden sm:flex"
+                } block sm:flex justify-between w-full`}
+              >
+                <div className="ml-0 sm:ml-10 block sm:flex items-baseline">
                   <NavLink
                     to="/movie/top_rated"
-                    className="hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                    className="block hover:text-white px-3 py-2 rounded-md text-sm font-medium mx-0 sm:mx-4"
                     activeClassName="bg-gray-900 text-white"
                     inactiveClassName="text-gray-300 hover:bg-gray-700"
                   >
@@ -48,7 +67,7 @@ const Navbar = () => {
 
                   <NavLink
                     to="/movie/upcoming"
-                    className="hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                    className="block hover:text-white px-3 py-2 rounded-md text-sm font-medium mx-0 sm:mx-4"
                     activeClassName="bg-gray-900 text-white"
                     inactiveClassName="text-gray-300 hover:bg-gray-700"
                   >
@@ -57,7 +76,7 @@ const Navbar = () => {
 
                   <NavLink
                     to="/movie/now_playing"
-                    className="hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                    className="block hover:text-white px-3 py-2 rounded-md text-sm font-medium mx-0 sm:mx-4"
                     activeClassName="bg-gray-900 text-white"
                     inactiveClassName="text-gray-300 hover:bg-gray-700"
                   >
@@ -66,7 +85,7 @@ const Navbar = () => {
 
                   <NavLink
                     to="/movie/popular"
-                    className="hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                    className="block hover:text-white px-3 py-2 rounded-md text-sm font-medium mx-0 sm:mx-4"
                     activeClassName="bg-gray-900 text-white"
                     inactiveClassName="text-gray-300 hover:bg-gray-700"
                   >
@@ -76,7 +95,7 @@ const Navbar = () => {
                   {account && (
                     <NavLink
                       to="/movie/watchlist"
-                      className="hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                      className="block hover:text-white px-3 py-2 rounded-md text-sm font-medium mx-0 sm:mx-4"
                       activeClassName="bg-gray-900 text-white"
                       inactiveClassName="text-gray-300 hover:bg-gray-700"
                     >
@@ -87,14 +106,14 @@ const Navbar = () => {
 
                 {account ? (
                   <span
-                    className="text-white hover:text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer"
+                    className="block text-white hover:text-white px-3 py-2 rounded-md text-sm font-medium mx-0 sm:mx-4 cursor-pointer"
                     onClick={() => onLogoutAccount()}
                   >
                     Logout {account.username}
                   </span>
                 ) : (
                   <span
-                    className="text-white hover:text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer"
+                    className="block text-white hover:text-white px-3 py-2 rounded-md text-sm font-medium mx-0 sm:mx-4 cursor-pointer"
                     onClick={() => setToggle(true)}
                   >
                     Login
