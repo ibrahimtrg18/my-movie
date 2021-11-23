f01e4f6a9acbd2a0ad8af7f0839770570bf460ce
Ibrahim Tarigan <ibrahimtarigan@gmail.com>
2021-11-23 22:50:14 +0700

    improving content footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,9 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      <footer></footer>
+      <footer className="bg-gray-200 text-gray-700 font-bold px-4 py-2">
+        Copyright 2021, Ibrahim Tarigan
+      </footer>
     </>
   );
 };
